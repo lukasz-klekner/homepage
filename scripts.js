5dd564bd3649857568fd4884167a88df2feaa4a7
Lukasz Klekner <lukaszklekner@gmail.com>
2020-05-11 09:29:54 +0200

    change the heading on homepage by using JS and add class to this heading

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 const name = "Łukasz";
 const age = 25;
 
-alert("Siemka!");
+//alert("Siemka!");
 console.log(`Man na imię ${name} i mam ${age} lat.`);
+
+const header = document.querySelector('.header__title--js');
+header.innerHTML="Łukasz Klekner - Moja pierwsza strona"
