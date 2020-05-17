4e108a2058a3396defe30ea8b160793a151c372e
Lukasz Klekner <lukaszklekner@gmail.com>
2020-05-17 09:49:47 +0200

    script: add functions greet and greet2

--- scripts.js
+++ scripts.js
@@ -2,7 +2,17 @@ const name = "Łukasz";
 const age = 25;
 
 //alert("Siemka!");
-console.log(`Man na imię ${name} i mam ${age} lat.`);
+//console.log(`Man na imię ${name} i mam ${age} lat.`);
 
 const header = document.querySelector('.header__title--js');
 header.innerHTML="Łukasz Klekner - Moja pierwsza strona"
+
+
+const greet = function (name, age){
+    console.log(`Mam na imię ${name}. W tym roku skończę ${age+1} lat.`);
+}
+
+const greet2 = (name, age) => console.log(`Mam na imię ${name}. W tym roku skończę ${age+1} lat.`);
+
+greet(name, age);
+greet2(name, age);
